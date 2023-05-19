9cc929d14d78431eeacc4b977dc4467de6abbcb9
CallumOB <callumobrien2004@gmail.com>
2023-05-19 16:12:06 +0100

    Created getters and setters for Publication

--- src/Publication.java
+++ src/Publication.java
@@ -32,4 +32,20 @@ public class Publication {
     public String toString() {
         return "Title: " + name + " Price: €" + price;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
